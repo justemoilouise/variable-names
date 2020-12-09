6996d94f03b6138520897702bd0e7e10d20ac1de
Louise Ann Apostol <louiseannapostol@MacBook-Pro.local>
2020-12-09 22:01:11 +0800

    add nav

--- client/components/header/header.tsx
+++ client/components/header/header.tsx
@@ -1,9 +1,26 @@
 import React from 'https://esm.sh/react';
+import { Link, useRouter } from "https://deno.land/x/aleph/mod.ts"
 import Logo from './logo.tsx';
 import './header.css';
 
 export default function Header() {
+    const { pathname } = useRouter();
+
+    const navItems = [
+        { label: 'Home', href: '/' },
+        { label: 'About', href: '/about' },
+    ];
+
     return (<div className="headerContainer">
         <Logo />
+        <nav className="headerContainerNav">
+            <ul className="headerContainerNavList">
+                {navItems.map((item, index) => (
+                    <li className={`headerContainerNavListItem ${item.href === pathname ? 'headerContainerNavListItem--active' : ''}`} active={} key={index}>
+                        <Link to={item.href}>{item.label}</Link>
+                    </li>
+                ))}
+            </ul>
+        </nav>
     </div>);
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,11 +1,16 @@
 import React from 'https://esm.sh/react';
+import { Import } from "https://deno.land/x/aleph/mod.ts"
 import Header from '../components/header/header.tsx';
 import Footer from '../components/footer/footer.tsx';
 
 export default function Home() {
     return (
         <React.Fragment>
+            <Import from="../style/index.less" />
             <Header />
+            <p>I am groot. I am groot.</p>
+            <p>I am groot. I am groot.</p>
+            <p>I am groot. I am groot.</p>
             <Footer />
         </React.Fragment>
     );

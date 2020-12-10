0fa34557177ae598aba60cbaf67701031af4c90c
Louise Ann Apostol <louiseannapostol@MacBook-Pro.local>
2020-12-10 20:30:08 +0800

    header updates

--- client/components/header/header.tsx
+++ client/components/header/header.tsx
@@ -11,16 +11,24 @@ export default function Header() {
         { label: 'About', href: '/about' },
     ];
 
-    return (<div className="headerContainer">
-        <Logo />
+    return (<header className="headerContainer">
+        <div className="headerContainerHeading">
+            <Logo />
+            <h1>Groot</h1>
+        </div>
         <nav className="headerContainerNav">
             <ul className="headerContainerNavList">
                 {navItems.map((item, index) => (
-                    <li className={`headerContainerNavListItem ${item.href === pathname ? 'headerContainerNavListItem--active' : ''}`} active={} key={index}>
+                    <li className={`headerContainerNavListItem ${item.href === pathname ? 'headerContainerNavListItem--active' : ''}`} key={index}>
                         <Link to={item.href}>{item.label}</Link>
                     </li>
                 ))}
             </ul>
+            <ul className="headerContainerNavList">
+                <li className="headerContainerNavListItem">
+                    <a href="https://github.com/justemoilouise/variable-names/" target="_blank">GitHub</a>
+                </li>
+            </ul>
         </nav>
-    </div>);
+    </header>);
 }
--- client/components/header/logo.tsx
+++ client/components/header/logo.tsx
@@ -1,6 +1,6 @@
 import React from 'https://esm.sh/react'
 
-export default function Logo({ width = 75 }: { width?: number }) {
+export default function Logo({ width = 64 }: { width?: number }) {
     return (
         <img src="/logo.svg" width={width} title="Aleph.js" />
     )
